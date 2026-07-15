13e11385fd879556b12bf12a5b919bc075f67542
afannasr <nasrafan98@gmail.com>
2026-07-15 13:45:55 +0500

    Day-1/day1.py

---
+++ Day-1/day1.py
@@ -0,0 +1,48 @@
+# ===========================
+# Day 1 - Python Basics
+# ===========================
+
+# 1. Print Statements
+print("Hello World")
+print("Welcome to Python")
+
+# 2. Variables
+name = "Afan"
+age = 23
+city = "Karak"
+is_student = True
+
+print(name)
+print(age)
+print(city)
+print(is_student)
+
+# 3. User Input
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("Name:", name)
+print("Age:", age)
+
+# 4. Data Types
+a = 10
+b = 23.4
+c = "Python"
+d = True
+e = None
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+# 5. Arithmetic Operators
+num1 = 23
+num2 = 23
+
+print("Addition =", num1 + num2)
+print("Subtraction =", num1 - num2)
+print("Multiplication =", num1 * num2)
+print("Division =", num1 / num2)
+print("Modulus =", num1 % num2)
