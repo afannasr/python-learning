459f15de68b13a4ecc5940f05625abd9bff3ccaa
afannasr <nasrafan98@gmail.com>
2026-07-15 21:37:20 +0500

    day1.py

---
+++ day1.py
@@ -0,0 +1,66 @@
+#first python program
+print("hello world")
+
+#variable
+name="Afan"
+age=23
+height=5.6
+print(name)
+print(age)
+print(height)
+
+       #data type
+       
+#Integer
+age=23
+print(age)
+
+#Float
+height=5.6
+print(height)
+
+#boolen
+isstudent=True
+print(isstudent)
+
+#string
+name="Afan"
+print(name)
+
+#checking type 
+print(type(name))
+
+#Taking user Input
+name= input("Enter your name:")
+print(name)
+
+#type casting
+age = int(input("Enter age: "))
+height = float(input("Enter height: "))
+
+#operator
+a=5
+b=3
+print(a+b)
+print(a-b)
+print(a%b)
+
+#printing multiple values
+name="Afan nasr"
+age=23
+print(name,age)
+
+#f_string
+print(f"My name is {name} and I am {age} years old.")
+
+#Add two number
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+print(a+b)
+
+#Find Average
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Average =", (a + b) / 2)
